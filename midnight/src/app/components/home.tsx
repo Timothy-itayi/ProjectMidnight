34c95832a12b5454f4c3b90c2141db6429def659
Timothy itayi <timmytam10@gmail.com>
2024-08-07 14:39:24 +1000

    add carousel-card, remove bento-card

--- midnight/src/app/components/home.tsx
+++ midnight/src/app/components/home.tsx
@@ -2,15 +2,12 @@
 import React, { useState } from "react";
 import { Sidebar, SidebarBody, SidebarLink } from "../../client-side-components/side-bar";
 import { IconShoppingCart, IconBrandTabler, IconInfoCircle, IconAddressBook } from "@tabler/icons-react";
-
- 
-
-
-
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "../../../utils/cn";
+import { BentoGrid, BentoGridItem } from "@/client-side-components/bento-card";
+import { Carousel } from "@/client-side-components/carousel-card";
 
 const Home = () => {
   const links = [
@@ -123,23 +120,70 @@ const Dashboard = () => {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
-            <div
-              key={"first-array" + i}
-              className="h-20 w-full rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
-        <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
-            <div
-              key={"second-array" + i}
-              className="h-full w-full rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
+  
+      
+        <div className="flex gap-1 flex-1">
+        <Carousel items={data.map(item => (
+          <div key={item.title}>
+            <Image src={item.src} alt={item.title} width={300} height={200} />
+            <div>{item.content}</div>
+          </div>
+        ))} />
         </div>
       </div>
-    </div>
+      </div>
+   
   );
 };
+const Content = () => {
+  return (
+    <>
+      {[...new Array(3).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content" + index}
+            className="bg-[#F5F5F7] p-8 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700">
+                Project Midnight 
+              </span>{" "}
+              Fast, Easy shopping for F1 gear and apparel
+            </p>
+            <Image
+              src="/placeholder/f1.jpeg"
+              alt="placeholder"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const data = [
+
+
+  {
+    category: "Product",
+    title: "Maps for your iPhone 15 Pro Max.",
+    src: '/placeholder/build.jpeg',
+    content: <Content />,
+  },
+  {
+    category: "iOS",
+    title: "Photography just got better.",
+    src: '/placeholder/build.jpeg',
+    content: <Content />,
+  },
+  {
+    category: "Hiring",
+    title: "Hiring for a Staff Software Engineer",
+    src: '/placeholder/build.jpeg',
+    content: <Content />,
+  },
+];
+
